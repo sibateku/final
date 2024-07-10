26faaa0091fd38dbb909ad5d44aca98cc8beb720
Akito Sakoh <146631166+sibateku@users.noreply.github.com>
2024-07-10 09:02:58 +0900

    トップ完成
    スマホ非対応

---
+++ js/photo.js
@@ -0,0 +1,20 @@
+$('.slider').slick({
+    fade: true,
+    autoplay: true,
+    autoplaySpeed: 3000,
+    speed:1000,
+    infinite: true,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    arrows: true,
+    prevArrow: '<div class="slick-prev"></div>',
+    nextArrow: '<div class="slick-next"></div>',
+    dots: true,
+    pauseOnFocus: false,
+    pauseOnHover: false,
+    pauseOnDotsHover: false,
+});
+
+$('.slider').on('touchmove', function(event, slick, currentSlide, nextSlide) {
+    $('.slider').slick('slickPlay');
+});
--- js/script.js
+++ js/script.js
@@ -10,11 +10,3 @@ window.addEventListener('load', function () {
         }
     })
 });
-
-$(function() {
-    $(window).scroll(function() {
-        $("nav.floating").stop().animate(
-            {"top": $(window).scrollTop() + 100},
-        500);
-    });
-});
